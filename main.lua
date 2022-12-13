433de99d55fee753217a906881c67ec92bd2db20
asegsehsh <110033911+asegsehsh@users.noreply.github.com>
2022-12-12 23:17:18 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,11 @@
 local place = game.PlaceId
 
 if place == 6679968919 then
-  loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Fly%20Race",true))()
-  elseif place == 258258996 then
-  loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%201",true))()
-  loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%202",true))()
-  loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%203",true))()
-
+    loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Fly%20Race",true))()
+elseif place == 258258996 then
+    loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%201",true))()
+    loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%202",true))()
+    loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%203",true))()
+elseif place == 6284583030 then
+    loadstring(game:HttpGet'https://raw.githubusercontent.com/RunDTM/ZeeroxHub/main/Loader.lua')()
 end
