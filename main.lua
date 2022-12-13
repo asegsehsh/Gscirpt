ba2298bdada00c88b394d420195b3a3c2f04b382
asegsehsh <110033911+asegsehsh@users.noreply.github.com>
2022-12-12 23:09:25 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
 local place = game.placeid
+
+if place == 6679968919 then
+  loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Fly%20Race",true))()
+  elseif place == 258258996 then
+  loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%201",true))()
+  loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%202",true))()
+  loadstring(game:HttpGet("https://github.com/asegsehsh/Gscirpt/blob/main/scripts/Miners%20Haven%203",true))()
+
+end
